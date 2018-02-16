ed187f29e8dc763f58975970f0ecdb06b0be436a
Paul Carrillo <paulcarrillo05@gmail.com>
2018-02-15 20:56:23 -0700

    Using vanilla JS to create a simple item creation tool that can filter, add, and delete list items

---
+++ main.js
@@ -0,0 +1,67 @@
+  var form  = document.getElementById('addForm');
+  var itemList  = document.getElementById('items');
+  var filter = document.getElementById('filter');
+
+  // Form submit event
+  form.addEventListener('submit', addItem);
+  // delete event
+  itemList.addEventListener('click', removeItem);
+  // filter event
+  filter.addEventListener('keyup', filterItems);
+
+  // Add Item
+  function addItem(e) {
+    e.preventDefault();
+
+    // Get input value
+    var newItem = document.getElementById('item').value;
+
+    // create new getelement
+    var li = document.createElement('li');
+    // Add class
+    li.className = 'list-group-item';
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    // create delete button element
+    var deleteBtn = document.createElement('button');
+
+    //Add classes to delete button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+    //Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    //Append button to the li
+    li.appendChild(deleteBtn);
+
+    //Append li to the list
+    itemList.appendChild(li);
+  }
+
+  // Remove Item function
+  function removeItem(e) {
+    if(e.target.classList.contains('delete')) {
+      if(confirm('Are You Sure?')) {
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+      }
+    }
+  }
+
+  // Filter Items function
+  function filterItems(e) {
+    // convert text to lowerCase
+    var text = e.target.value.toLowerCase();
+    //Get li's
+    var items = itemList.getElementsByTagName('li');
+    // convert to an array
+    Array.from(items).forEach(function(item) {
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
